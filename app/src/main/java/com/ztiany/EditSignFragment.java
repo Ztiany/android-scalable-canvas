9d793b43d4a769d7abd4669aaf616dffad04cfbb
ztiany <ztiany3@gmail.com>
2016-11-28 23:38:18 +0800

    show undo menu

--- app/src/main/java/com/ztiany/EditSignFragment.java
+++ app/src/main/java/com/ztiany/EditSignFragment.java
@@ -42,6 +42,7 @@ public class EditSignFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setHasOptionsMenu(true);
 
     }
 
